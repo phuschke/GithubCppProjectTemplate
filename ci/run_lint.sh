cfec7baa615bf2011268872cf4669e4f4fa8e1b2
Philip Huschke <philiphuschke@gmail.com>
2017-09-01 15:28:16 +0200

    :sparkles:

--- ci/run_lint.sh
+++ ci/run_lint.sh
@@ -4,7 +4,7 @@
 set -e
 sudo apt-get install -y python3-pip
 echo "After python3 install ***************************************************"
-sudo -H install --upgrade pip
+sudo -H pip install --upgrade pip
 echo "After pip ***************************************************"
 sudo -H pip3 install coala-bears
 echo "After pip3 coala ***************************************************"
