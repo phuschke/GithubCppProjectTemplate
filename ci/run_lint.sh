4e5cf01dc5776bcefcbe95f7f1bd572be8dd241c
Philip Huschke <philiphuschke@gmail.com>
2017-09-01 15:47:44 +0200

    :sparkles:

--- ci/run_lint.sh
+++ ci/run_lint.sh
@@ -2,10 +2,4 @@
 #!/usr/bin/env bash
 
 set -e
-sudo apt-get install -y python3-pip
-echo "After python3 install ***************************************************"
-sudo -H pip install --upgrade pip
-echo "After pip ***************************************************"
-sudo -H pip3 install coala-bears
-echo "After pip3 coala ***************************************************"
-coala
+sudo docker run -ti -v $(pwd):/app --workdir=/app coala/base coala
